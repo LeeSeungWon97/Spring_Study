79616cb7b9953abc777f40c7981068bcb7ce3939
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-07-25 14:46:47 +0900

    객체 지향 원리 적용
    - 정률할인정책(RateDiscountPolicy) 코드 추가
    - AppConfig 추가로 인한 구조 수정
    - 기존 코드들을 SRP, OCP, DIP를 만족하는 코드로 수정

---
+++ core/src/main/java/com/hello/core/OrderApp.java
@@ -0,0 +1,12 @@
+package com.hello.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+    }
+}
--- core/src/test/java/com/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/com/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,22 @@
 package com.hello.core.member;
 
+import com.hello.core.AppConfig;
+import com.hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+    @BeforeEach
+    public void beforeEach(){
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        memberService = applicationContext.getBean("memberService", MemberService.class);
+    }
     @Test
     void join(){
         // given
