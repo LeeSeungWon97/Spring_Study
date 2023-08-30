ce5575edf3c00a792e00542de47b0bd2e3fc2fc0
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-08-31 00:55:54 +0900

    Servlet 학습
    Servlet 등록, HttpServletRequest & HttpServletResponse, GET 쿼리 파라미터로 HTTP 데이터 전송

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -6,13 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-/**
- * 1. 파라미터 전송 가능
- * http://localhost:8080/request-param?username=hello&age=20
- *
- * 2. 동일한 파라미터 전송 가능
- * http://localhost:8080/request-param?username=hello&username=world&age=20
-*/
 
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
@@ -20,28 +13,25 @@ public class RequestParamServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
-                        .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
-
         System.out.println();
 
         System.out.println("[단일 파라미터 조회]");
         String username = request.getParameter("username");
-        String age = request.getParameter("age");
-
         System.out.println("username = " + username);
-        System.out.println("age = " + age);
 
+        String age = request.getParameter("age");
+        System.out.println("age = " + age);
         System.out.println();
 
         System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
         String[] usernames = request.getParameterValues("username");
-        for(String name: usernames){
-            System.out.println("name = " + name);
+        for(String name : usernames) {
+            System.out.println("usernames = " + name);
         }
 
         response.getWriter().write("ok");
-
-
     }
 }
