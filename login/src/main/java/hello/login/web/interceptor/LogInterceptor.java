95326693a0c1f9067036f94dd7643ec054aa11c8
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-10-25 16:59:11 +0900

    Filter & Interceptor
    서블릿 필터와 스프링 인터셉트를 학습하고, 로그인 후 이용 가능한 서비스를 구분하는 예제

---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,49 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString(); //요청 로그를 구분하기 위한 uuid 생성
+
+        // preHandle 에서 지정한 값을 postHandle, afterCompletion 에서 함께 사용하기 위해 request 에 담아주기
+        request.setAttribute(LOG_ID, uuid);
+
+        //@RequestMapping: HandlerMethod
+        //정적 리소스: ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보 포함
+        }
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true; // true : 정상호출. 다음 인터셉트나 컨트롤러 호출, false : 진행 중지
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        Object logId = request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
