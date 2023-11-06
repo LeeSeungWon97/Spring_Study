125af7d13c2c715efa441e69260fa95627f69ce3
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-11-06 16:56:55 +0900

    JDBC
    JDBC를 사용하여 데이터베이스와의 연결 및 sql 전달 방법 학습

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    // Database에 접속하는데 필요한 기본 정보
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
