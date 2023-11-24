fb64e7ece2dab423bdccd7a7a3e316bcc84254af
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-11-24 12:31:55 +0900

    테스트케이스 데이터베이스 연동 방법
    테스트 케이스를 작성할 때 데이터베이스를 연동하는 방법에 대해 학습
    - @Transactional
    - 임베디드 모드 DB
    - @SpringBootTest

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,18 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
